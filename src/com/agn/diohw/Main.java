c4751bffd3aec9c7a421cb7f99a6de9e3a2db9da
gnedov <andrey.gnedov@gmail.com>
2014-06-25 19:18:09 +0300

    refactored HomeWork part1. added homeWork part2 ( array with POJO objest)

--- src/com/agn/diohw/Main.java
+++ src/com/agn/diohw/Main.java
@@ -3,8 +3,13 @@ package com.agn.diohw;
 public class Main {
 
     public static void main(String[] arg0) {
-/* HomeWork. Part1.
-     Создайте POJO класс, поля которого являются final (Person)
+        pojoPerson();  // HomeWork. Part1.
+        arrayPersons(); //  HomeWork. Part2.
+    }
+
+    private static void pojoPerson(){
+        /* HomeWork. Part1.
+    Создайте POJO класс, поля которого являются final (Person)
 Создайте Enum который характеризует экзмпляр POJO класса (Должность:DIRECTOR, DEVELOPER, QA)
 Добавьте Enum как поле в POJO
 Cгенерируйте equals/hashcode методы
@@ -20,13 +25,6 @@ Cгенерируйте equals/hashcode методы
         //compare persons with ==, .equals, and hashCode
         comparePersons(personIvan, personPetro);
         comparePersons(clone_1, clone_2);
-
-/* HomeWork. Part2.
-     Создайте два массива элементами которых Являются ранее созданные Pojo объекты
-Создайте Main класс, в main методе которого проведите распечатку в консоль и сравнение двух масивов
-         */
-        // TODO
-
     }
 
     private static void comparePersons(Person p1, Person p2) {
@@ -47,5 +45,34 @@ Cгенерируйте equals/hashcode методы
             System.out.println("_>Persons are equal!");
         else
             System.out.println("_>Persons are not equal!");
+    }
+
+    private static void arrayPersons(){
+        /* HomeWork. Part2.
+    Создайте два массива элементами которых Являются ранее созданные Pojo объекты
+Создайте Main класс, в main методе которого проведите распечатку в консоль и сравнение двух масивов
+        */
+        // Init unique persons
+        Person p1 = new Person("Ivan", "Ivanov", 23, EnumRank.TESTER);
+        Person p2 = new Person("Petro", "Petrov", 27, EnumRank.SELLER);
+        Person p3 = new Person("Igor", "Komarov", 28, EnumRank.DEVELOPER);
+        Person p4 = new Person("Victor", "Lomov", 29, EnumRank.DEVELOPER);
+        Person p5 = new Person("Eugene", "Koval", 23, EnumRank.TESTER);
+        Person p6 = new Person("Sidor", "Kozlov", 33, EnumRank.MANAGER);
+        Person p7 = new Person("Anna", "Zaharova", 19, EnumRank.DIRECTOR);
+        Person p8 = new Person("Olga", "Sidorchuk", 52, EnumRank.CLEANER);
+
+        Person[] arrFirstOffice = new Person[] {p1,p2,p3,p4,p8};
+        Person[] arrSecondOffice = new Person[] {p2,p5,p6,p7,p8};
+
+        for (Person i : arrFirstOffice){
+            for (Person j : arrSecondOffice){
+                if (i != null && j != null){
+                    if (i.equals(j))
+                        System.out.printf("Person %s %s %s works in both offices.%n",
+                                i.getRank(), i.getFirstName(), i.getLastName());
+                }
+            }
+        }
     }
 }
